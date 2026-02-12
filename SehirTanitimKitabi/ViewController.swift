3a64b1916034cc8605cbca905ccd61f3f0c9308d
aysusenn0 <aysue25@gmail.com>
2026-02-12 23:33:31 +0300

    Initial Commit

---
+++ SehirTanitimKitabi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SehirTanitimKitabi
+//
+//  Created by Aysu on 12.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
